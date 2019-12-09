323d838fe22a30ad4d656c3463ff1b66d7bf97a1
cshark-dev <asteroidzeta@gmail.com>
2019-12-09 11:09:22 +0000

    Properties for Employee model added.

--- Payway.Entity/Employee.cs
+++ Payway.Entity/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Payway.Entity
@@ -7,8 +8,11 @@ namespace Payway.Entity
     public class Employee
     {
         public int Id { get; set; }
+        [Required]             
         public string EmployeeNo { get; set; }
+        [Required,  MaxLength(50)]
         public string FirstName { get; set; }
+        [Required, MaxLength(50)]
         public string MiddleName { get; set; }
         public  string LastName { get; set; }
         public string FullName { get; set; }
@@ -18,12 +22,15 @@ namespace Payway.Entity
         public DateTime DateJoined { get; set; }
         public string Designation { get; set; }
         public string Email { get; set; }
+        [Required, MaxLength(50)]
         public string NationalInsuranceNo { get; set; }
         public PaymentMethod Paymentmethod { get; set; }
         public StudentLoan StudentLoan { get; set; }
         public UnionMember UnionMember { get; set; }
+        [Required, MaxLength(150)]
         public string Address { get; set; }
         public string City { get; set; }
+        [Required, MaxLength(50)]
         public string Postcode { get; set; }
         public IEnumerable<PaymentRecord> PaymentRecords { get; set; }
 
